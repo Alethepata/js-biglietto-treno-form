1ae0eaac1334c5bb89af97f07f3536d52066f9f7
Alethepata <alessia.patane@gmail.com>
2023-09-08 15:40:02 +0200

    [Edit] script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+1. Salvare in 2 variabili i km e l'età.
+2. Moltiplicare i km per 0,21. 
+3. Creare 3 IF: -sconto minorenni(-20%) -sconto over 65(-40%) -prezzo base.
+4. Generare dei numeri random per "Carrozza" e " Codice CP".
+5. Creare la stringa di output.
+6. Effettuare l'innerHTML dell'output nel tag designato.
+7. Al click del bottone "genera" aggiungo la classe active alla "2° row - Il tuo biglietto ".
+8. Al click del bottone "annulla" rimuovo la classe active alla "2° row - Il tuo biglietto ".
+*/ 
